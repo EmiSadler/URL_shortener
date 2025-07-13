39b2ab9261300a56a25621ed6925cf0c6a08b250
Emily Sadler <emily@sadler.gg>
2025-07-13 11:49:53 +0100

    moved db init to create_app because of gunicorn

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from app.routes import register_routes
 def create_app():
     """Application factory"""
     app = Flask(__name__, static_folder='/app/static/static', static_url_path='/static')
-    
+    init_db()
     # Enable CORS for all routes (for frontend development)
     CORS(app)
     
@@ -18,7 +18,7 @@ def create_app():
 
 def main():
     """Initialize database and start the application"""
-    init_db()
+    # Initialize the database in create_app instead because of gunicorn
     app = create_app()
     port = int(os.getenv("PORT", 8000))
     app.run(host='0.0.0.0', port=port, debug=True)
